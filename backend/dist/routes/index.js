48ec57dcda584912f237da823dc66c182fec77a4
Sai Khant Min Bhone <saikhantminbhone@gmail.com>
2025-07-12 17:10:41 +0700

    remove node modules

--- backend/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const recommendation_controller_1 = require("../controllers/recommendation.controller");
-const router = (0, express_1.Router)();
-router.post('/recommendation', recommendation_controller_1.createRecommendation);
-exports.default = router;
